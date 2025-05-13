f5b077e89c60d95471a080fbf1805771ecb93091
Hyun <jjjtoy@naver.com>
2025-05-13 17:36:41 +0900

    fix: 테이블 총 생성 수(totalTableCreateCount)

--- models/Visiter.js
+++ models/Visiter.js
@@ -2,67 +2,62 @@ const mongoose = require("mongoose");
 const moment = require("moment-timezone");
 
 const visiterSchema = new mongoose.Schema({
-  date: {
-    type: String,
-    required: true,
-    unique: true,
-    default: () => moment().tz("Asia/Seoul").format("YYYY-MM-DD"),
-  },
-  todayVisitCreatePage: { type: Number, default: 0 },
-  totalVisitCreatePage: { type: Number, default: 0 },
+     date: {
+          type: String,
+          required: true,
+          unique: true,
+          default: () => moment().tz("Asia/Seoul").format("YYYY-MM-DD"),
+     },
+     todayVisitCreatePage: { type: Number, default: 0 },
+     totalVisitCreatePage: { type: Number, default: 0 },
 
-  todayVisitAboutPage: { type: Number, default: 0 },
-  totalVisitAboutPage: { type: Number, default: 0 },
+     todayVisitAboutPage: { type: Number, default: 0 },
+     totalVisitAboutPage: { type: Number, default: 0 },
 
-  todayVisitUsePage: { type: Number, default: 0 },
-  totalVisitUsePage: { type: Number, default: 0 },
+     todayVisitUsePage: { type: Number, default: 0 },
+     totalVisitUsePage: { type: Number, default: 0 },
 
-  todayTableCreateCount: { type: Number, default: 0 },
-  totalTableCreateCount: { type: Number, default: 0 },
+     todayTableCreateCount: { type: Number, default: 0 },
+     totalTableCreateCount: { type: Number, default: 0 },
 
-  todaySignUp: { type: Number, default: 0 },
-  totalSignUp: { type: Number, default: 0 },
+     todaySignUp: { type: Number, default: 0 },
+     totalSignUp: { type: Number, default: 0 },
 
-  todayLogin: { type: Number, default: 0 },
-  totalLogin: { type: Number, default: 0 },
+     todayLogin: { type: Number, default: 0 },
+     totalLogin: { type: Number, default: 0 },
 });
 
 visiterSchema.pre("save", async function (next) {
-  if (this.isNew) {
-    return next();
-  }
+     if (this.isNew) {
+          return next();
+     }
 
-  try {
-    const previousData = await mongoose.model("Visiter").findOne({ date: this.date });
+     try {
+          const previousData = await mongoose.model("Visiter").findOne({ date: this.date });
 
-    if (previousData) {
-      if (this.isModified("todayVisitCreatePage")) {
-        this.totalVisitCreatePage = previousData.totalVisitCreatePage + 1;
-      }
-      if (this.isModified("todayVisitAboutPage")) {
-        this.totalVisitAboutPage = previousData.totalVisitAboutPage + 1;
-      }
-      if (this.isModified("todayVisitUsePage")) {
-        this.totalVisitUsePage = previousData.totalVisitUsePage + 1;
-      }
+          if (previousData) {
+               if (this.isModified("todayVisitCreatePage")) {
+                    this.totalVisitCreatePage = previousData.totalVisitCreatePage + 1;
+               }
+               if (this.isModified("todayVisitAboutPage")) {
+                    this.totalVisitAboutPage = previousData.totalVisitAboutPage + 1;
+               }
+               if (this.isModified("todayVisitUsePage")) {
+                    this.totalVisitUsePage = previousData.totalVisitUsePage + 1;
+               }
 
-      if (this.isModified("todaySignUp")) {
-        this.totalSignUp = previousData.totalSignUp + 1;
-      }
-      if (this.isModified("todayLogin")) {
-        this.totalLogin = previousData.totalLogin + 1;
-      }
+               if (this.isModified("todaySignUp")) {
+                    this.totalSignUp = previousData.totalSignUp + 1;
+               }
+               if (this.isModified("todayLogin")) {
+                    this.totalLogin = previousData.totalLogin + 1;
+               }
+          }
+     } catch (error) {
+          return next(error);
+     }
 
-      // 📌 테이블 생성 횟수 업데이트
-      if (this.isModified("todayTableCreateCount")) {
-        this.totalTableCreateCount = previousData.totalTableCreateCount + 1;
-      }
-    }
-  } catch (error) {
-    return next(error);
-  }
-
-  next();
+     next();
 });
 
 const Visiter = mongoose.model("Visiter", visiterSchema);
